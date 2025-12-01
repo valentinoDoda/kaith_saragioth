ed5adb267fad37604fd177305657963f893cc36b
unknown <valododa123@gmail.com>
2025-12-01 17:03:03 +0200

    Finish with the pop up

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const pop_up = document.getElementsByClassName("menu_popup");
 pop_up[0].addEventListener("click", ()=> {
-    pop_up[0].style.display ="none";
+    pop_up[0].classList.replace("show", "hide");
+})
+
+const showupMenu = document.getElementById("showupMenu");
+showupMenu.addEventListener("click", ()=> {
+    pop_up[0].classList.replace("hide", "show");
 })
