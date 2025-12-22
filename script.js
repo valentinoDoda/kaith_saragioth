38d945f7083ea00568057f1e1bf79333b9a52144
valentin doda <valododa123@gmail.com>
2025-12-22 18:44:43 +0200

    asd

--- script.js
+++
@@ -1,9 +0,0 @@
-const pop_up = document.getElementsByClassName("menu_popup");
-pop_up[0].addEventListener("click", ()=> {
-    pop_up[0].classList.replace("show", "hide");
-})
-
-const showupMenu = document.getElementById("showupMenu");
-showupMenu.addEventListener("click", ()=> {
-    pop_up[0].classList.replace("hide", "show");
-})
